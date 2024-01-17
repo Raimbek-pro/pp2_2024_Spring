3b468afaa077d79c3d14a5b52e24e66ee95fab76
Райымбек Омаров <raimbekq75@gmail.com>
2024-01-17 18:57:32 +0600

    data types in python

---
+++ lab1/python data types/setting_a_data_type.py
@@ -0,0 +1,16 @@
+x = "Hello World"  # str
+x = 20 #int
+x = 20.5 #float 
+x = 1j #complex
+x = ["apple", "banana", "cherry"] #list
+x = ("apple", "banana", "cherry") #tuple
+x = range(6) #range
+x = {"name" : "John", "age" : 36} #dict
+x = {"apple", "banana", "cherry"} #set 
+x = frozenset({"apple", "banana", "cherry"}) #frozen set
+x = True #bool
+x = b"Hello" #bytes
+x = bytearray(5) # byte  array of size 5 initially set to 0
+x = memoryview(bytes(5)) #memoryview
+x = None # NoneType
+
