65adc37f8d272d09449e34e456f77ad2f478d215
Райымбек Омаров <raimbekq75@gmail.com>
2024-01-27 15:47:35 +0600

    dictionaries added

---
+++ lab2/dictionaries/methods/ex1.py
@@ -0,0 +1,19 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.setdefault("model", "Bronco")
+
+print(x)
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.setdefault("color", "white")
+
+print(x)
---
+++ lab2/dictionaries/methods/ex3.py
@@ -0,0 +1,22 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.keys()
+
+print(x)
+
+
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+x = car.keys()
+
+car["color"] = "white"
+
+print(x)
